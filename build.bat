7e8044cbeb06ff2656dca2fb9b053e183b700c73
lansinuote <45538336@qq.com>
2025-06-09 11:01:21 +0800

    commit.

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-pyinstaller --onefile --windowed --clean --noconfirm --add-binary "libs/ffmpeg.exe;libs" --add-data "libs/large-v3-turbo.pt;libs" --collect-data whisper --name=ÀëÏß×ÖÄ»Ê¶±ð ui.py
+pyinstaller --onefile --windowed --clean --noconfirm --add-binary "libs/ffmpeg.exe;libs" --add-data "libs/large-v3-turbo.pt;libs" --collect-data whisper --name=Offline_Subtitle_Generator ui.py
